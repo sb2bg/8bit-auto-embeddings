46bd4259731a60ba45f0ac1e497ce2c7c953fcb4
sb2bg <sullivanbognar@gmail.com>
2024-04-15 19:30:56 -0500

    Add tqdm library for progress bar in chroma_embedder.py

--- chroma_embedder.py
+++ chroma_embedder.py
@@ -1,26 +1,32 @@
 import chromadb
 import csv
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+import tqdm
 
+print("Loading db...")
 client = chromadb.PersistentClient(path="chroma.db")
 
 # specify the embedding function to use
+print("Loading embedder...")
 embedder = SentenceTransformerEmbeddingFunction(
     model_name="thenlper/gte-small",
     # specify the device to use, e.g. "cuda" or "cpu"
     device="cuda",
 )
 
+print("Creating/getting collection...")
 collection = client.get_or_create_collection(name="cars", embedding_function=embedder)
 
+print("Loading data...")
 with open("data.csv") as f:
     reader = csv.DictReader(f)
     data = list(reader)
 
 documents = [item["excerpt"] for item in data]
-ids = [str(i) for i in range(len(documents))]
 
-if len(documents) != len(data) or len(documents) != len(ids):
+if len(documents) != len(data):
     raise ValueError("Documents, data, and ids must have the same length")
 
-collection.add(documents=documents, metadatas=data, ids=ids)
+print("Adding documents...")
+for i, doc in tqdm.tqdm(enumerate(documents), total=len(documents)):
+    collection.add(ids=str(i), documents=doc, metadatas=data[i])
